269c68dd1d5e2a6828d9a53694b03426de60e332
Nata Silva <nata.mw@hotmail.com>
2020-06-04 00:45:29 -0300

    Add list issues in Repository page

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,6 +1,7 @@
-import React, {useState, FormEvent} from 'react';
+import React, {useState, FormEvent, useEffect} from 'react';
 import {Title, Form, Repositories, Error} from './styles';
 import {FiChevronRight} from 'react-icons/fi';
+import { Link } from 'react-router-dom';
 import logo from '../../assets/logo.svg';
 import api from '../../services/api';
 
@@ -17,8 +18,22 @@ const Dashboard: React.FC = () => {
   const [newRepo, setNewRepo] = useState('');
   const [inputError, setInputError] = useState('');
 
-  const [repositories, setRepositories] = useState<Repository[]>([]);
+  const [repositories, setRepositories] = useState<Repository[]>(() => {
+    const storageRepositories = localStorage.getItem('@GithubRepositories');
 
+    if(storageRepositories) {
+      return JSON.parse(storageRepositories)
+    }else{
+      return []
+    }
+  });
+
+  useEffect(() => {
+    localStorage.setItem(
+      '@GithubRepositories',
+      JSON.stringify(repositories),
+      );
+  }, [repositories])
   async function handleAddReposirory(event: FormEvent<HTMLFormElement>): Promise<void> {
     event.preventDefault();
 
@@ -57,7 +72,7 @@ const Dashboard: React.FC = () => {
 
     <Repositories>
      {repositories.map(repository => (
-        <a key={repository.full_name} href="">
+      <Link key={repository.full_name} to={`repositories/${repository.full_name}`}>
         <img src={repository.owner.avatar_url} alt={repository.owner.login}/>
         <div>
           <strong>{repository.full_name} </strong>
@@ -65,7 +80,7 @@ const Dashboard: React.FC = () => {
         </div>
 
         <FiChevronRight size={20}/>
-      </a>
+      </Link>
      ))}
     </Repositories>
   </>
